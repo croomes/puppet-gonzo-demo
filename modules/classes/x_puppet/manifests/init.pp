e26a941fc4313c06a000fc7a829ef9241a89389c
Simon Croome <simon@croome.org>
2014-03-01 11:02:00 +0000

    Refactor x_factor module

--- modules/classes/x_puppet/manifests/init.pp
+++ modules/classes/x_puppet/manifests/init.pp
@@ -1,13 +1,17 @@
 class x_puppet (
-  # $certname = $::fqdn,
-  # $archive_file_server = $::fqdn,
+  $certname = $::hostname,
   $puppet_server = 'puppet',
 ) {
+
   file { '/etc/puppetlabs/puppet/puppet.conf':
     ensure  => file,
     content => template('x_puppet/puppet-agent.conf.erb'),
     owner   => 'pe-puppet',
     group   => 'pe-puppet',
   }
-}
 
+  file { '/etc/profile.d/puppet.sh':
+    ensure  => file,
+    content => 'PATH=$PATH:/opt/puppet/bin'
+  }
+}
