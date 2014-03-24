df5685d21d2247525412b9346e42d8dae6b3c2d7
Simon Croome <simon@croome.org>
2014-03-24 01:58:38 +0000

    Improvements after rebuilding from docs

--- modules/classes/x_puppet/bin/bootstrap.sh
+++ modules/classes/x_puppet/bin/bootstrap.sh
@@ -5,23 +5,27 @@
 [[ -d /etc/puppetlabs/puppet/environments/production ]] || mkdir /etc/puppetlabs/puppet/environments/production
 
 # Symlink demo modules for development
-[[ -d /vagrant/puppet-gonzo-demo ]] && ln -s /vagrant/puppet-gonzo-demo/* /etc/puppetlabs/puppet/environments/development
+[[ -d /etc/puppetlabs/puppet/environments/development ]] || \
+  ln -s /vagrant/puppet-gonzo-demo /etc/puppetlabs/puppet/environments/development
+
+# Disable firewall
+chkconfig iptables off
+service iptables stop
 
 # Set path so gem provider is found
 export PATH=$PATH:/opt/puppet/bin
 
 # Initial run to configure hiera
+FACTER_release=development \
 puppet apply \
-   --modulepath=/etc/puppetlabs/puppet/environments/production/modules/classes \
-   -e 'include x_puppet::master'
+  --modulepath=/etc/puppetlabs/puppet/environments/development/modules/classes \
+  -e 'include x_puppet::master'
+
+# Remove old site.pp
+[[ -f /etc/puppetlabs/puppet/manifests/site.pp ]] && rm /etc/puppetlabs/puppet/manifests/site.pp
 
 # Hiera changes need to be picked up
 service pe-httpd restart
 
 # Apply the rest of the role, assigned in site.pp
 puppet agent -t --server $(hostname -f)
-
-# Remove default module structure
-# TODO: was causing problems with Passenger
-# [[ -d /etc/puppetlabs/puppet/modules ]] && rm -rf /etc/puppetlabs/puppet/modules
-# [[ -d /etc/puppetlabs/puppet/manifests ]] && rm -rf /etc/puppetlabs/puppet/manifests
